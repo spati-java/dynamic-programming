d69e3a90324aea8700794c6ef14709fbd59a8ab9
Sourav Pati <sourav.pati09@gmail.com>
2020-12-11 11:54:50 -0600

    first file

---
+++ java-script/fib.js
@@ -0,0 +1,11 @@
+const fib = (number , memo={}) => {
+
+    if(number in memo) return memo[number];
+    if (number <= 2)
+        return 1;
+        
+    memo[number] = fib(number - 1 , memo) + fib(number - 2 , memo);
+    return memo[number];
+}
+
+console.log(fib(50));
