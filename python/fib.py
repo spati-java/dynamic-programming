53dc6fd355d15cdce2dac1837843f2d1a2b6905f
Sourav Pati <sourav.pati09@gmail.com>
2020-12-12 12:03:57 -0600

    fib using python

--- python/fib.py
+++ python/fib.py
@@ -1,4 +1,11 @@
-def my_function():
-  print("Hello from a function")
+def fib (number , memo = {}):
+  if number in memo:
+    return memo.get(number)
+  if number <= 2: 
+    return 1
+    
+  memo[number] = fib(number-1) + fib(number-2)
 
-my_function()
+  return memo.get(number);
+
+print(fib(50))
