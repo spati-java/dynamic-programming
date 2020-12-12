880029a15d5382941bfcff4ef4fcf3c03a7fbd87
Sourav Pati <sourav.pati09@gmail.com>
2020-12-12 09:11:35 -0600

    dynamic programming using python

---
+++ java/src/main/java/FibonachiWithMemorization.java
@@ -0,0 +1,27 @@
+import java.util.HashMap;
+
+public class FibonachiWithMemorization {
+
+    HashMap<Integer, Integer> map = new HashMap<>();
+
+    public static void main(String[] args) {
+
+        FibonachiWithMemorization fibonachiWithMemorization = new FibonachiWithMemorization();
+
+        System.out.println(fibonachiWithMemorization.fib(8));
+
+    }
+
+    public int fib(int number ){
+
+        if(this.map.containsKey(number))
+            return this.map.get(number);
+
+        if (number <= 2) return 1;
+
+        this.map.put(number , fib(number -1 ) + fib(number -2));
+
+        return this.map.get(number);
+
+    }
+}
